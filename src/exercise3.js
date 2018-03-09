c6a7ae2705c34645d182f7d5a120e49ceb0710f8
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2018-03-09 17:33:06 +0100

    Solution exercise 3

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,5 +1,18 @@
-var applyMap = function() {
-  // Your future job begins here ...
+var applyMap = function(inputValues, inputMaps) {
+  var result = []
+  if (inputMaps) {
+    for (let i = 0; i < inputMaps.length; i++) {
+      var mapFunction = inputMaps[i]
+      var mapResult = []
+      for (let j = 0; j < inputValues.length; j++) {
+        var value = inputValues[j]
+        var valueMapped = mapFunction(value)
+        mapResult.push(valueMapped)
+      }
+      result.push(mapResult)
+    }
+  }
+  return result
 }
 
 module.exports = {
